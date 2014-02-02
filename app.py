05e62ba113acf95b443017263e8753a2d3d159f8
Matt Zimmerman <mdz@heroku.com>
2014-02-02 14:03:30 -0800

    Get ready for wordpress bits

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 import os
 import feedparser
 import jinja2
+import xmlrpclib
 
 # TODO:
-# - only show links which are new since the last draft was posted
 # - post the draft to Wordpress
+# - only show links which are new since the last draft was posted
 
 from flask import Flask
 app = Flask(__name__)
 
-feed_urls = ['http://feeds.delicious.com/v2/rss/tag/geekfeminism',
-             'http://feeds.pinboard.in/rss/t:geekfeminism']
+wordpress_url = os.environ['WORDPRESS_URL']
+wordpress_user = os.environ['WORDPRESS_USER']
+wordpress_password = os.environ['WORDPRESS_PASSWORD']
+feed_urls = os.environ['FEEDS'].split()
+
+def get_feeds():
+  return map(feedparser.parse, feed_urls)
 
 @app.route('/')
 def preview():
   template = jinja2.Template(open('preview.html').read())
-  feeds = map(feedparser.parse, feed_urls)
-  return template.render(feeds=feeds)
+  return template.render(feeds=get_feeds())
 
 @app.route('/post-draft')
 def post_draft(reset=False):
   template = jinja2.Template(open('wordpress.html').read())
-  feeds = map(feedparser.parse, feed_urls)
-  post_body = template.render(feeds=feeds)
+  post_body = template.render(feeds=get_feeds())
+  return ''
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
