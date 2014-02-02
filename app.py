d5a9bf34b2225898664f9982008f6c51d1b2c1ff
Matt Zimmerman <mdz@heroku.com>
2014-02-02 13:32:20 -0800

    Add templating

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
+import feedparser
+from jinja2 import Template
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def dumpenv():
-    return '<br>'.join(['%s=%s' % (k, v) for k, v in os.environ.items()])
+def dump():
+  template = Template(open('template.html').read())
+  return template.render(links=[{'href': 'http://www.example.com', 'title': 'Example'}])
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
